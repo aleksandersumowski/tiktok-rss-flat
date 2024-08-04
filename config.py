2211f4883794808bf99e1e72ae5e9ae01cc42569
Aleksander Sumowski <github@sumowski.pl>
2024-08-04 22:43:40 +0200

    setup

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
 # Set your GH Pages URL here e.g.:
-ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
-ghRawURL = "https://raw.githubusercontent.com/conoro/tiktok-rss-flat/main/"
-
-# ghPagesURL = "https://tiktokrss.conoroneill.com/"
+ghPagesURL = "https://aleksandersumowski.github.io/tiktok-rss-flat/"
+ghRawURL = "https://raw.githubusercontent.com/aleksandersumowski/tiktok-rss-flat/main/"
